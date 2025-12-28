a5c1fac22ea7f6ba77a7f756eeae98b02e9c4ed1
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2025-12-28 00:01:49 -0800

    -

--- .sh
+++ .sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 set -euo pipefail
-rm -rf apps bin logs qb64 2> /dev/null || :
-mkdir -p apps bin logs logs/build
+flag() {
+	for f in "$@"
+		do [[ -e ".flags/$f" ]] || return 1
+	done
+}
+rm -rf bin logs 2> /dev/null || :
+mkdir -p bin logs logs/build
 curl \
 	https://github.com/QB64Official/qb64/releases/download/v2.1/qb64_dev_2022-09-08-07-14-00_47f5044_lnx.tar.gz \
 	-L \
-	-o apps/qb64.tar.gz
-# tar -xf apps/qb64.tar.gz &> logs/qb64.log
-ln -sf "$HOME/Apps/qb64/qb64" qb64
+	--output qb64.tar.gz
+tar -xf qb64.tar.gz --transform="s|^[^/]*|qb64|"
 while read -r s
 	do
 		b=${s%.bas}
 		b=${b#src/}
-		log=logs/build/$b.log
-		./qb64 -x $s -o "$HOME/Projects/basic-game/bin/$b" &> $log || cat $log && sleep 5
-		# ./bin/$b || :
+		./qb64/qb64 -x $s -o "$HOME/Projects/basic-game/bin/$b"
+		if flag local
+			then ./bin/$b || :
+		fi
 done < <(find src -name \*.bas)
-find . -empty -o -name "qb64" -delete
+find . -empty -delete
